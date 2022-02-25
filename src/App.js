52315cff34b7c9b2c967035cdbab1b9f5e28987c
Tyler <hachadorian98@gmail.com>
2022-02-25 13:10:49 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchData } from "./helpers/fetchData";
+import jsonData from "./helpers/object.json";
 
 const App = () => {
+  const [geojson, setGeoJson] = useState(null);
+
+  useEffect(() => {
+    // fetchData().then((res) => {
+    //   setGeoJson(res);
+    // });
+
+    // testing
+    setGeoJson(jsonData);
+  }, []);
+
   return <div className="App"></div>;
 };
 
---
+++ src/helpers/fetchData.js
@@ -0,0 +1,8 @@
+export const fetchData = async () => {
+  const req =
+    await fetch(`https://arcgis.dvrpc.org/portal/rest/services/Demographics/IPD_2019/FeatureServer/0/query?wher
+e=1%3D1&geometryPrecision=5&outfields=ipd_score,d_score,em_score,f_score,fb_score,lep_score
+,li_score,oa_score,rm_score,y_score&f=geojson`);
+  const res = await req.json();
+  return res;
+};
